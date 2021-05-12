fe67519d4c4c60eb195fc843dbe46cda5cd9340e
Dario <mangilidario@gmail.com>
2021-05-12 22:31:32 +0200

    ciao

--- model/saved.js
+++ model/saved.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const saved = new Schema({
-    nome : {type : String, required: true},
     ricette : {type : Array, required: true}
 });
 module.exports = mongoose.model('saved',saved);
